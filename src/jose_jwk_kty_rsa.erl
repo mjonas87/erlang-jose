467a559dce4da66519f00cbfc690ecef8b35cf68
Matt Jonas <matt.jonas@gmail.com>
2017-11-13 18:28:59 -0700

    Stuff'

--- src/jose_jwk_kty_rsa.erl
+++ src/jose_jwk_kty_rsa.erl
@@ -203,8 +203,8 @@ encrypt_public(PlainText, Options, #'RSAPrivateKey'{modulus=Modulus, publicExpon
 %%====================================================================
 
 sign(Message, JWSALG, RSAPrivateKey=#'RSAPrivateKey'{}) ->
-	io:format("TURKEYS!!!!!!!!!!!!!!!!!!!!!!!!");
-	io:format(Message);
+	io:fwrite("TURKEYS!!!!!!!!!!!!!!!!!!!!!!!!").
+	io:fwrite(Message).
 
 	{Padding, DigestType} = jws_alg_to_digest_type(JWSALG),
 	jose_jwa:sign(Message, DigestType, RSAPrivateKey, Padding).
